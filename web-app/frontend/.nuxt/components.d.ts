466477ee2ec3e53d7ca05357e36cbf6ed02eff16
kennethchen2b04 <kenneth.21@ichat.sp.edu.sg>
2025-07-12 00:48:57 +0800

    [feat] python3 fix

--- web-app/frontend/.nuxt/components.d.ts
+++ web-app/frontend/.nuxt/components.d.ts
@@ -14,6 +14,7 @@ type LazyComponent<T> = (T & DefineComponent<HydrationStrategies, {}, {}, {}, {}
 interface _GlobalComponents {
       'ConfigPanel': typeof import("../components/ConfigPanel.vue")['default']
     'HeaderBar': typeof import("../components/HeaderBar.vue")['default']
+    'InvestigationPanel': typeof import("../components/InvestigationPanel.vue")['default']
     'LiveIncidentPanel': typeof import("../components/LiveIncidentPanel.vue")['default']
     'Main': typeof import("../components/Main.vue")['default']
     'TranscriptionPanel': typeof import("../components/TranscriptionPanel.vue")['default']
@@ -33,6 +34,10 @@ interface _GlobalComponents {
     'AccordionContent': typeof import("../components/ui/accordion/index")['AccordionContent']
     'AccordionItem': typeof import("../components/ui/accordion/index")['AccordionItem']
     'AccordionTrigger': typeof import("../components/ui/accordion/index")['AccordionTrigger']
+    'Alert': typeof import("../components/ui/alert/index")['Alert']
+    'AlertDescription': typeof import("../components/ui/alert/index")['AlertDescription']
+    'AlertTitle': typeof import("../components/ui/alert/index")['AlertTitle']
+    'Badge': typeof import("../components/ui/badge/index")['Badge']
     'Card': typeof import("../components/ui/card/index")['Card']
     'CardAction': typeof import("../components/ui/card/index")['CardAction']
     'CardContent': typeof import("../components/ui/card/index")['CardContent']
@@ -40,27 +45,23 @@ interface _GlobalComponents {
     'CardFooter': typeof import("../components/ui/card/index")['CardFooter']
     'CardHeader': typeof import("../components/ui/card/index")['CardHeader']
     'CardTitle': typeof import("../components/ui/card/index")['CardTitle']
-    'Alert': typeof import("../components/ui/alert/index")['Alert']
-    'AlertDescription': typeof import("../components/ui/alert/index")['AlertDescription']
-    'AlertTitle': typeof import("../components/ui/alert/index")['AlertTitle']
+    'Button': typeof import("../components/ui/button/index")['Button']
     'Carousel': typeof import("../components/ui/carousel/index")['Carousel']
     'CarouselContent': typeof import("../components/ui/carousel/index")['CarouselContent']
     'CarouselItem': typeof import("../components/ui/carousel/index")['CarouselItem']
     'CarouselNext': typeof import("../components/ui/carousel/index")['CarouselNext']
     'CarouselPrevious': typeof import("../components/ui/carousel/index")['CarouselPrevious']
     'CarouselApi': typeof import("../components/ui/carousel/index")['CarouselApi']
-    'Button': typeof import("../components/ui/button/index")['Button']
-    'Badge': typeof import("../components/ui/badge/index")['Badge']
     'Dialog': typeof import("../components/ui/dialog/index")['Dialog']
     'DialogContent': typeof import("../components/ui/dialog/index")['DialogContent']
     'DialogHeader': typeof import("../components/ui/dialog/index")['DialogHeader']
     'DialogTitle': typeof import("../components/ui/dialog/index")['DialogTitle']
     'DialogTrigger': typeof import("../components/ui/dialog/index")['DialogTrigger']
     'Input': typeof import("../components/ui/input/index")['Input']
-    'Textarea': typeof import("../components/ui/textarea/index")['Textarea']
-    'Switch': typeof import("../components/ui/switch/index")['Switch']
-    'Progress': typeof import("../components/ui/progress/index")['Progress']
     'Label': typeof import("../components/ui/label/index")['Label']
+    'Progress': typeof import("../components/ui/progress/index")['Progress']
+    'Switch': typeof import("../components/ui/switch/index")['Switch']
+    'Textarea': typeof import("../components/ui/textarea/index")['Textarea']
     'ColorScheme': typeof import("../node_modules/@nuxtjs/color-mode/dist/runtime/component.vue3.vue")['default']
     'NuxtPage': typeof import("../node_modules/nuxt/dist/pages/runtime/page")['default']
     'NoScript': typeof import("../node_modules/nuxt/dist/head/runtime/components")['NoScript']
@@ -76,6 +77,7 @@ interface _GlobalComponents {
     'NuxtRouteAnnouncer': IslandComponent<typeof import("../node_modules/nuxt/dist/app/components/server-placeholder")['default']>
       'LazyConfigPanel': LazyComponent<typeof import("../components/ConfigPanel.vue")['default']>
     'LazyHeaderBar': LazyComponent<typeof import("../components/HeaderBar.vue")['default']>
+    'LazyInvestigationPanel': LazyComponent<typeof import("../components/InvestigationPanel.vue")['default']>
     'LazyLiveIncidentPanel': LazyComponent<typeof import("../components/LiveIncidentPanel.vue")['default']>
     'LazyMain': LazyComponent<typeof import("../components/Main.vue")['default']>
     'LazyTranscriptionPanel': LazyComponent<typeof import("../components/TranscriptionPanel.vue")['default']>
@@ -95,6 +97,10 @@ interface _GlobalComponents {
     'LazyAccordionContent': LazyComponent<typeof import("../components/ui/accordion/index")['AccordionContent']>
     'LazyAccordionItem': LazyComponent<typeof import("../components/ui/accordion/index")['AccordionItem']>
     'LazyAccordionTrigger': LazyComponent<typeof import("../components/ui/accordion/index")['AccordionTrigger']>
+    'LazyAlert': LazyComponent<typeof import("../components/ui/alert/index")['Alert']>
+    'LazyAlertDescription': LazyComponent<typeof import("../components/ui/alert/index")['AlertDescription']>
+    'LazyAlertTitle': LazyComponent<typeof import("../components/ui/alert/index")['AlertTitle']>
+    'LazyBadge': LazyComponent<typeof import("../components/ui/badge/index")['Badge']>
     'LazyCard': LazyComponent<typeof import("../components/ui/card/index")['Card']>
     'LazyCardAction': LazyComponent<typeof import("../components/ui/card/index")['CardAction']>
     'LazyCardContent': LazyComponent<typeof import("../components/ui/card/index")['CardContent']>
@@ -102,27 +108,23 @@ interface _GlobalComponents {
     'LazyCardFooter': LazyComponent<typeof import("../components/ui/card/index")['CardFooter']>
     'LazyCardHeader': LazyComponent<typeof import("../components/ui/card/index")['CardHeader']>
     'LazyCardTitle': LazyComponent<typeof import("../components/ui/card/index")['CardTitle']>
-    'LazyAlert': LazyComponent<typeof import("../components/ui/alert/index")['Alert']>
-    'LazyAlertDescription': LazyComponent<typeof import("../components/ui/alert/index")['AlertDescription']>
-    'LazyAlertTitle': LazyComponent<typeof import("../components/ui/alert/index")['AlertTitle']>
+    'LazyButton': LazyComponent<typeof import("../components/ui/button/index")['Button']>
     'LazyCarousel': LazyComponent<typeof import("../components/ui/carousel/index")['Carousel']>
     'LazyCarouselContent': LazyComponent<typeof import("../components/ui/carousel/index")['CarouselContent']>
     'LazyCarouselItem': LazyComponent<typeof import("../components/ui/carousel/index")['CarouselItem']>
     'LazyCarouselNext': LazyComponent<typeof import("../components/ui/carousel/index")['CarouselNext']>
     'LazyCarouselPrevious': LazyComponent<typeof import("../components/ui/carousel/index")['CarouselPrevious']>
     'LazyCarouselApi': LazyComponent<typeof import("../components/ui/carousel/index")['CarouselApi']>
-    'LazyButton': LazyComponent<typeof import("../components/ui/button/index")['Button']>
-    'LazyBadge': LazyComponent<typeof import("../components/ui/badge/index")['Badge']>
     'LazyDialog': LazyComponent<typeof import("../components/ui/dialog/index")['Dialog']>
     'LazyDialogContent': LazyComponent<typeof import("../components/ui/dialog/index")['DialogContent']>
     'LazyDialogHeader': LazyComponent<typeof import("../components/ui/dialog/index")['DialogHeader']>
     'LazyDialogTitle': LazyComponent<typeof import("../components/ui/dialog/index")['DialogTitle']>
     'LazyDialogTrigger': LazyComponent<typeof import("../components/ui/dialog/index")['DialogTrigger']>
     'LazyInput': LazyComponent<typeof import("../components/ui/input/index")['Input']>
-    'LazyTextarea': LazyComponent<typeof import("../components/ui/textarea/index")['Textarea']>
-    'LazySwitch': LazyComponent<typeof import("../components/ui/switch/index")['Switch']>
-    'LazyProgress': LazyComponent<typeof import("../components/ui/progress/index")['Progress']>
     'LazyLabel': LazyComponent<typeof import("../components/ui/label/index")['Label']>
+    'LazyProgress': LazyComponent<typeof import("../components/ui/progress/index")['Progress']>
+    'LazySwitch': LazyComponent<typeof import("../components/ui/switch/index")['Switch']>
+    'LazyTextarea': LazyComponent<typeof import("../components/ui/textarea/index")['Textarea']>
     'LazyColorScheme': LazyComponent<typeof import("../node_modules/@nuxtjs/color-mode/dist/runtime/component.vue3.vue")['default']>
     'LazyNuxtPage': LazyComponent<typeof import("../node_modules/nuxt/dist/pages/runtime/page")['default']>
     'LazyNoScript': LazyComponent<typeof import("../node_modules/nuxt/dist/head/runtime/components")['NoScript']>
@@ -144,6 +146,7 @@ declare module 'vue' {
 
 export const ConfigPanel: typeof import("../components/ConfigPanel.vue")['default']
 export const HeaderBar: typeof import("../components/HeaderBar.vue")['default']
+export const InvestigationPanel: typeof import("../components/InvestigationPanel.vue")['default']
 export const LiveIncidentPanel: typeof import("../components/LiveIncidentPanel.vue")['default']
 export const Main: typeof import("../components/Main.vue")['default']
 export const TranscriptionPanel: typeof import("../components/TranscriptionPanel.vue")['default']
@@ -163,6 +166,10 @@ export const Accordion: typeof import("../components/ui/accordion/index")['Accor
 export const AccordionContent: typeof import("../components/ui/accordion/index")['AccordionContent']
 export const AccordionItem: typeof import("../components/ui/accordion/index")['AccordionItem']
 export const AccordionTrigger: typeof import("../components/ui/accordion/index")['AccordionTrigger']
+export const Alert: typeof import("../components/ui/alert/index")['Alert']
+export const AlertDescription: typeof import("../components/ui/alert/index")['AlertDescription']
+export const AlertTitle: typeof import("../components/ui/alert/index")['AlertTitle']
+export const Badge: typeof import("../components/ui/badge/index")['Badge']
 export const Card: typeof import("../components/ui/card/index")['Card']
 export const CardAction: typeof import("../components/ui/card/index")['CardAction']
 export const CardContent: typeof import("../components/ui/card/index")['CardContent']
@@ -170,27 +177,23 @@ export const CardDescription: typeof import("../components/ui/card/index")['Card
 export const CardFooter: typeof import("../components/ui/card/index")['CardFooter']
 export const CardHeader: typeof import("../components/ui/card/index")['CardHeader']
 export const CardTitle: typeof import("../components/ui/card/index")['CardTitle']
-export const Alert: typeof import("../components/ui/alert/index")['Alert']
-export const AlertDescription: typeof import("../components/ui/alert/index")['AlertDescription']
-export const AlertTitle: typeof import("../components/ui/alert/index")['AlertTitle']
+export const Button: typeof import("../components/ui/button/index")['Button']
 export const Carousel: typeof import("../components/ui/carousel/index")['Carousel']
 export const CarouselContent: typeof import("../components/ui/carousel/index")['CarouselContent']
 export const CarouselItem: typeof import("../components/ui/carousel/index")['CarouselItem']
 export const CarouselNext: typeof import("../components/ui/carousel/index")['CarouselNext']
 export const CarouselPrevious: typeof import("../components/ui/carousel/index")['CarouselPrevious']
 export const CarouselApi: typeof import("../components/ui/carousel/index")['CarouselApi']
-export const Button: typeof import("../components/ui/button/index")['Button']
-export const Badge: typeof import("../components/ui/badge/index")['Badge']
 export const Dialog: typeof import("../components/ui/dialog/index")['Dialog']
 export const DialogContent: typeof import("../components/ui/dialog/index")['DialogContent']
 export const DialogHeader: typeof import("../components/ui/dialog/index")['DialogHeader']
 export const DialogTitle: typeof import("../components/ui/dialog/index")['DialogTitle']
 export const DialogTrigger: typeof import("../components/ui/dialog/index")['DialogTrigger']
 export const Input: typeof import("../components/ui/input/index")['Input']
-export const Textarea: typeof import("../components/ui/textarea/index")['Textarea']
-export const Switch: typeof import("../components/ui/switch/index")['Switch']
-export const Progress: typeof import("../components/ui/progress/index")['Progress']
 export const Label: typeof import("../components/ui/label/index")['Label']
+export const Progress: typeof import("../components/ui/progress/index")['Progress']
+export const Switch: typeof import("../components/ui/switch/index")['Switch']
+export const Textarea: typeof import("../components/ui/textarea/index")['Textarea']
 export const ColorScheme: typeof import("../node_modules/@nuxtjs/color-mode/dist/runtime/component.vue3.vue")['default']
 export const NuxtPage: typeof import("../node_modules/nuxt/dist/pages/runtime/page")['default']
 export const NoScript: typeof import("../node_modules/nuxt/dist/head/runtime/components")['NoScript']
@@ -206,6 +209,7 @@ export const NuxtIsland: typeof import("../node_modules/nuxt/dist/app/components
 export const NuxtRouteAnnouncer: IslandComponent<typeof import("../node_modules/nuxt/dist/app/components/server-placeholder")['default']>
 export const LazyConfigPanel: LazyComponent<typeof import("../components/ConfigPanel.vue")['default']>
 export const LazyHeaderBar: LazyComponent<typeof import("../components/HeaderBar.vue")['default']>
+export const LazyInvestigationPanel: LazyComponent<typeof import("../components/InvestigationPanel.vue")['default']>
 export const LazyLiveIncidentPanel: LazyComponent<typeof import("../components/LiveIncidentPanel.vue")['default']>
 export const LazyMain: LazyComponent<typeof import("../components/Main.vue")['default']>
 export const LazyTranscriptionPanel: LazyComponent<typeof import("../components/TranscriptionPanel.vue")['default']>
@@ -225,6 +229,10 @@ export const LazyAccordion: LazyComponent<typeof import("../components/ui/accord
 export const LazyAccordionContent: LazyComponent<typeof import("../components/ui/accordion/index")['AccordionContent']>
 export const LazyAccordionItem: LazyComponent<typeof import("../components/ui/accordion/index")['AccordionItem']>
 export const LazyAccordionTrigger: LazyComponent<typeof import("../components/ui/accordion/index")['AccordionTrigger']>
+export const LazyAlert: LazyComponent<typeof import("../components/ui/alert/index")['Alert']>
+export const LazyAlertDescription: LazyComponent<typeof import("../components/ui/alert/index")['AlertDescription']>
+export const LazyAlertTitle: LazyComponent<typeof import("../components/ui/alert/index")['AlertTitle']>
+export const LazyBadge: LazyComponent<typeof import("../components/ui/badge/index")['Badge']>
 export const LazyCard: LazyComponent<typeof import("../components/ui/card/index")['Card']>
 export const LazyCardAction: LazyComponent<typeof import("../components/ui/card/index")['CardAction']>
 export const LazyCardContent: LazyComponent<typeof import("../components/ui/card/index")['CardContent']>
@@ -232,27 +240,23 @@ export const LazyCardDescription: LazyComponent<typeof import("../components/ui/
 export const LazyCardFooter: LazyComponent<typeof import("../components/ui/card/index")['CardFooter']>
 export const LazyCardHeader: LazyComponent<typeof import("../components/ui/card/index")['CardHeader']>
 export const LazyCardTitle: LazyComponent<typeof import("../components/ui/card/index")['CardTitle']>
-export const LazyAlert: LazyComponent<typeof import("../components/ui/alert/index")['Alert']>
-export const LazyAlertDescription: LazyComponent<typeof import("../components/ui/alert/index")['AlertDescription']>
-export const LazyAlertTitle: LazyComponent<typeof import("../components/ui/alert/index")['AlertTitle']>
+export const LazyButton: LazyComponent<typeof import("../components/ui/button/index")['Button']>
 export const LazyCarousel: LazyComponent<typeof import("../components/ui/carousel/index")['Carousel']>
 export const LazyCarouselContent: LazyComponent<typeof import("../components/ui/carousel/index")['CarouselContent']>
 export const LazyCarouselItem: LazyComponent<typeof import("../components/ui/carousel/index")['CarouselItem']>
 export const LazyCarouselNext: LazyComponent<typeof import("../components/ui/carousel/index")['CarouselNext']>
 export const LazyCarouselPrevious: LazyComponent<typeof import("../components/ui/carousel/index")['CarouselPrevious']>
 export const LazyCarouselApi: LazyComponent<typeof import("../components/ui/carousel/index")['CarouselApi']>
-export const LazyButton: LazyComponent<typeof import("../components/ui/button/index")['Button']>
-export const LazyBadge: LazyComponent<typeof import("../components/ui/badge/index")['Badge']>
 export const LazyDialog: LazyComponent<typeof import("../components/ui/dialog/index")['Dialog']>
 export const LazyDialogContent: LazyComponent<typeof import("../components/ui/dialog/index")['DialogContent']>
 export const LazyDialogHeader: LazyComponent<typeof import("../components/ui/dialog/index")['DialogHeader']>
 export const LazyDialogTitle: LazyComponent<typeof import("../components/ui/dialog/index")['DialogTitle']>
 export const LazyDialogTrigger: LazyComponent<typeof import("../components/ui/dialog/index")['DialogTrigger']>
 export const LazyInput: LazyComponent<typeof import("../components/ui/input/index")['Input']>
-export const LazyTextarea: LazyComponent<typeof import("../components/ui/textarea/index")['Textarea']>
-export const LazySwitch: LazyComponent<typeof import("../components/ui/switch/index")['Switch']>
-export const LazyProgress: LazyComponent<typeof import("../components/ui/progress/index")['Progress']>
 export const LazyLabel: LazyComponent<typeof import("../components/ui/label/index")['Label']>
+export const LazyProgress: LazyComponent<typeof import("../components/ui/progress/index")['Progress']>
+export const LazySwitch: LazyComponent<typeof import("../components/ui/switch/index")['Switch']>
+export const LazyTextarea: LazyComponent<typeof import("../components/ui/textarea/index")['Textarea']>
 export const LazyColorScheme: LazyComponent<typeof import("../node_modules/@nuxtjs/color-mode/dist/runtime/component.vue3.vue")['default']>
 export const LazyNuxtPage: LazyComponent<typeof import("../node_modules/nuxt/dist/pages/runtime/page")['default']>
 export const LazyNoScript: LazyComponent<typeof import("../node_modules/nuxt/dist/head/runtime/components")['NoScript']>
